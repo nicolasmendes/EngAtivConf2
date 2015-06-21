54099a71319ebf4e4ff08d1da2a034d89e10e606
NICOLAS <NICOLAS@NICOLAS-PC>
2015-06-20 23:21:26 -0300

    versao 1.0

--- src/engativconf2/A.java
+++ src/engativconf2/A.java
@@ -5,6 +5,16 @@ public class A {
 
     private int A1;
 
+    private String A3;
+
+    public String getA3() {
+        return A3;
+    }
+
+    public void setA3(String A3) {
+        this.A3 = A3;
+    }
+
     public int getA1() {
         return A1;
     }
